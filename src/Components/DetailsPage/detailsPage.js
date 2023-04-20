25324cd236c3f609a80a171b93d7349915615ddf
Noor Aldeen <anooraldeen9@gmail.com>
2023-04-20 15:16:03 +0200

    improve detailes page

--- src/Components/DetailsPage/detailsPage.js
+++ src/Components/DetailsPage/detailsPage.js
@@ -11,28 +11,20 @@ const baseURL = "https://fakestoreapi.com/products";
 const DetailsPage = (props) => {
   const params = useParams();
   const [product, setProduct] = useState({});
-  const [selected, setSelected] = useState(null);
   useEffect(() => {
     axios.get(baseURL).then((response) => {
       const data = response.data.filter((el) => el.id == params.id);
       setProduct(data[0]);
     });
   }, []);
-  const textTitle = {
-    color: "#000000",
-    fontWeight: "400",
 
-    marginRight: "1em",
-    opacity: "0.5",
-  };
-
-  if(!selected) return <></>
+  if(!product) return <></>
   return (
     <div className={styles.content}>
       <Box>
-        <PathActive category={selected.category} title={selected.title}/>
+        <PathActive category={product.category} title={product.title}/>
       </Box>
-      <ProductAttributes Product={selected}/>
+      <ProductAttributes Product={product}/>
       <RelatedItems product={params.id} />
 
     </div>
